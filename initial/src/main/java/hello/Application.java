abbda7b960b572f5a5f627a783acd5944134a808
Greg Turnquist <gturnquist@pivotal.io>
2018-03-01 09:54:57 -0600

    Upgrade to Spring Boot 2.0.0.RELEASE

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -2,9 +2,6 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.bind.RelaxedPropertyResolver;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Application {
