b617ab6750917c725b6e076706850c0fdfdcb84d
Greg Turnquist <gturnquist@pivotal.io>
2019-11-26 13:09:27 -0600

    Switch to tabs, and adjust tabsizing in asciidoctor.

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
-    @RequestMapping("/")
-    public String home() {
-        return "Hello Docker World";
-    }
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
 }
