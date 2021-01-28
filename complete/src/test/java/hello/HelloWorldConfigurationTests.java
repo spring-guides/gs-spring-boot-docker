8533591c636d19f8cfe9435600b753d1ee82df83
Jay Bryant <jbryant@pivotal.io>
2021-01-28 13:50:47 -0600

    Upgrade to Boot 2.4.2
    Keeping the build files current and adding deep links to the Intializr.

--- complete/src/test/java/hello/HelloWorldConfigurationTests.java
+++ complete/src/test/java/hello/HelloWorldConfigurationTests.java
@@ -15,10 +15,11 @@
  */
 package hello;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -27,9 +28,8 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @DirtiesContext
 public class HelloWorldConfigurationTests {
